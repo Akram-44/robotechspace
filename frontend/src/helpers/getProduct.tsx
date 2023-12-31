9dd9f5b1c869e657d3f0dc17c338bd934aae3f46
Akram A. Abdelbasir <ashrfakrm64@gmail.com>
2023-12-31 19:11:41 +0200

    update

--- frontend/src/helpers/getProduct.tsx
+++ frontend/src/helpers/getProduct.tsx
@@ -1,6 +1,6 @@
 export async function getProduct(id) {
     try {
-      const res = await fetch(`http://127.0.0.1:1337/api/products?populate=*/${id}`);
+      const res = await fetch(`http://127.0.0.1:1337/api/products/${id}?populate=*`);
       const response = await res.json();
       return response.data;
     } catch (error) {
